80fa8f8e52af076185f2bb2ad43a925379d2f049
SevralT <a.chycherin@outlook.com>
2022-03-24 16:46:49 +0000

    chore: Remove WfdCommon for now

--- common.mk
+++ common.mk
@@ -453,8 +453,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
