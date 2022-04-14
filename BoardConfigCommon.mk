077cf792212aa5623e86229e7f0cebd6ecf07f61
SevralT <a.chycherin@outlook.com>
2022-04-14 07:18:11 +0000

    sdm845-common: Allow OEM unlocking & enable partitions verification

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -144,8 +144,6 @@ TARGET_TRUST_USB_CONTROL_DISABLE := 1
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
